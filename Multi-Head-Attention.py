ee47fdd55c0381ade495a9f10ff9a4c6c31e658c
tianyalidi <yalidi.tian@gmail.com>
2026-05-27 13:13:56 +0800

    Update Multi-Head-Attention.py

--- Multi-Head-Attention.py
+++ Multi-Head-Attention.py
@@ -22,5 +22,40 @@ class MultiHeadAttention(nn.Module):
   def forward(self, x, mask = None):
     B, T, D = x.shape
 
-    Q = Q.view(B, T, self.nums_head, self.d_k)
-    
+    Q = Q.view(B, T, self.num_heads, self.d_k).transpose(1, 2)
+    K = K.view(B, T, self.num_heads, self.d_k).transpose(1, 2)
+    V = V.view(B, T, self.num_heads, self.d_k).transpose(1, 2)
+
+    scores = torch.matmul(Q, K.transpose(-2, -1))
+
+    scores = scores / math.sqrt(self.d_k)
+
+    if mask is not None:
+      scores = scores.masked_fill(mask == 0, float('-inf))
+
+      attn = F.softmax(scores, dim=-1)
+
+      out = torch.matmul(attn, V)
+
+      out = out.transpose(1, 2).comtiguous().view(B, T, D)
+
+      out = self.W_o(out)
+
+      return out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
